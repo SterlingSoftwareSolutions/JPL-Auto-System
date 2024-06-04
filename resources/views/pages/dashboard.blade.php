c4492fec02e4e92714bf6365dd8251d5fc39e454
DilshadMr360 <mohomeddilshad123@gmail.com>
2024-06-04 12:30:14 +0530

    header UI changes

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="flex justify-center items-center h-screen">
+
+    <!-- Custom CSS for background image -->
+    <div class="custom-bg"></div>
+
+</div>
+@endsection
+
+
+
