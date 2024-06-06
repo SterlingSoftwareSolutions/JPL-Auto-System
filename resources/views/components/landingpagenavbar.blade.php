9074c5ff6eebc8ed6035155fcbe17f161dc2b1d3
DilshadMr360 <mohomeddilshad123@gmail.com>
2024-06-06 10:43:01 +0530

    landing page header with icons and fully response

--- resources/views/components/landingpagenavbar.blade.php
+++ resources/views/components/landingpagenavbar.blade.php
@@ -14,29 +14,51 @@
         <!-- Container for left-aligned items -->
 <div class="flex justify-start items-start gap-5 bg-white">
     @if(Route::currentRouteName() === 'vehicledetailspage')
-        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000 ">Vehicle Details</a>
-    @else
-        <a href="{{ route('vehicledetailspage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">Vehicle Details</a>
+      <a href="#"  class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000">  <i class="fa-solid fa-car mr-2"></i>Vehicle Details</a>
+        @else
+        <a href="{{ route('vehicledetailspage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">
+            <i class="fa-solid fa-car mr-2 @if(Route::currentRouteName() === 'vehicledetailspage') text-white  @endif"></i>
+            Vehicle Details
+        </a>
     @endif
     @if(Route::currentRouteName() === 'workingstructions')
-        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000 ">Work Instructions</a>
+        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000"> <i class="fa-solid fa-bullhorn mr-2"></i>Work Instructions</a>
     @else
-        <a href="{{ route('workingstructions') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">Work Instructions</a>
+
+    <a href="{{ route('workingstructions') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">
+        <i class="fa-solid fa-bullhorn mr-2 @if(Route::currentRouteName() === 'workingstructions') text-white  @endif"></i>
+        Work Instructions
+    </a>
     @endif
+
     @if(Route::currentRouteName() === 'compliancepage')
-        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000">Compliance</a>
+        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000"><i class="fa-solid fa-sun mr-2"></i>Compliance</a>
     @else
-        <a href="{{ route('compliancepage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">Compliance</a>
+    <a href="{{ route('compliancepage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">
+        <i class="fa-solid fa-sun mr-2 @if(Route::currentRouteName() === 'compliancepage') text-white  @endif"></i>
+        Compliance
+    </a>
     @endif
+
+
     @if(Route::currentRouteName() === 'partlistpage')
-        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000">Parts List</a>
+        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000"><i class="fa-solid fa-bars mr-2"></i> Parts List</a>
     @else
-        <a href="{{ route('partlistpage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">Parts List</a>
+
+    <a href="{{ route('partlistpage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">
+        <i class="fa-solid fa-bars mr-2 @if(Route::currentRouteName() === 'partlistpage') text-white  @endif"></i>
+        Parts List
+    </a>
     @endif
+
+
     @if(Route::currentRouteName() === 'supplierspage')
-        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000">Suppliers</a>
+        <a href="#" class="text-white w-44 h-16 flex justify-center items-center font-bold bg-black hover:bg-black hover:text-white transition duration-1000"> <i class="fa-solid fa-user mr-2"></i>Suppliers</a>
     @else
-        <a href="{{ route('supplierspage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">Suppliers</a>
+    <a href="{{ route('supplierspage') }}" class="text-black w-44 h-16 flex justify-center items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000">
+        <i class="fa-solid fa-user mr-2 @if(Route::currentRouteName() === 'supplierspage') text-white  @endif"></i>
+        Suppliers
+    </a>
     @endif
 </div>
 
@@ -44,26 +66,70 @@
 
             <!-- Spacer -->
             <div class="flex-grow"></div>
+
             <!-- Container for right-aligned item -->
-            <div class="flex justify-end items-end ">
-                <a href="{{route('dashboard')}}" class="text-black hover:text-orange-700 w-44 h-16 flex justify-end items-center font-bol ">Dashboard</a>
-            </div>
+
+             <div class="items-center flex justify-center hover:text-orange-700 transition duration-500">
+    <div class="flex items-center justify-center">
+        <i class="fa-regular fa-rectangle-xmark"></i>
+    </div>
+
+    <div class="flex justify-end items-end w-full">
+        <a href="{{ route('dashboard') }}" class="text-black w-full h-16 flex justify-end items-center  hover:text-orange-500 transition duration-500">Dashboard</a>
+    </div>
+</div>
+
+
+
 
         </div>
 
     </div>
 
 
-    <div x-show="open" class="md:hidden">
-        <ul class="flex flex-col ">
-            <li><a href="#" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">Vehicle Details</a></li>
-            <li><a href="#" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">Work Instructions</a></li>
-            <li><a href="#" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">Compliance</a></li>
-            <li><a href="#" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">Parts List</a></li>
-            <li><a href="#" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">Suppliers</a></li>
-            <li><a href="#" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">Dashboard</a></li>
-        </ul>
-    </div>
+   <div x-show="open" class="md:hidden">
+    <ul class="flex flex-col">
+        <li class="">
+            <a href="{{ route('vehicledetailspage') }}" class="text-black w-full h-16 flex items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">
+                <i class="fa-solid fa-car mr-2"></i>
+                <span class="flex-1 text-left">Vehicle Details</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('workingstructions') }}" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">
+                <i class="fa-solid fa-bullhorn mr-2 "></i>
+                <span class="flex-1 text-left">Work Instructions</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('compliancepage') }}" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">
+                <i class="fa-solid fa-sun mr-2 "></i>
+                <span class="flex-1 text-left">Compliance</span>
+
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('partlistpage') }}" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">
+                <i class="fa-solid fa-bars mr-2 "></i>
+                <span class="flex-1 text-left">Parts List</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('supplierspage') }}" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">
+                <i class="fa-solid fa-user mr-2 "></i>
+                <span class="flex-1 text-left">Suppliers</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('dashboard') }}" class="text-black w-full h-16 flex justify-between items-center font-bold bg-white hover:bg-black hover:text-white transition duration-1000 pl-8 pr-6">
+                <i class="fa-regular fa-rectangle-xmark mr-2 "></i>
+                <span class="flex-1 text-left">Dashboard</span>
+            </a>
+        </li>
+    </ul>
+</div>
+
 
 
 </nav>
