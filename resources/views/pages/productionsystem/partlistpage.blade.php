b8249dcab95025668063cc505facd5f43b7022f5
Alpha Azhar <66483078+alphaazhar2000@users.noreply.github.com>
2024-07-02 09:28:24 +0530

    Update partlistpage.blade.php

--- resources/views/pages/productionsystem/partlistpage.blade.php
+++ resources/views/pages/productionsystem/partlistpage.blade.php
@@ -239,8 +239,8 @@
                                             d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z" />
                                     </svg>
                                 </td>
-                                <td class="px-8 py-2 ">{{ $labourPart->price }} </td>
-                                <td class="px-8 py-2 ">{{ $labourPart->supplier }}</td>
+                                <td class="px-8 py-2  max-w-xs break-words">{{ $labourPart->price }} </td>
+                                <td class="px-8 py-2  max-w-xs break-words">{{ $labourPart->supplier }}</td>
 
 
 
