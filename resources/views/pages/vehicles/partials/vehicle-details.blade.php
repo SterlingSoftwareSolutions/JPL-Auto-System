30a178356b2461b6cc5432525d36b65df9b9dc49
Dhasindu Nethmika <dhasindunethmika22@gmail.com>
2026-08-16 14:24:33 +0530

    create vehicle details partial with core specifications table and 9-grid image layout

--- resources/views/pages/vehicles/partials/vehicle-details.blade.php
+++ resources/views/pages/vehicles/partials/vehicle-details.blade.php
@@ -466,8 +466,7 @@
                                             <svg class="w-6 h-6 text-[#b4975a]" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
                                         </div>
                                         <div>
-                                            <h3 class="text-xl font-bold text-gray-900">{{ $category->name }} Specifications</h3>
-                                            <p class="text-sm text-gray-500 mt-1">Purpose-built. Performance-driven.</p>
+                                            <h3 class="text-xl font-bold text-gray-900 uppercase">{{ $category->name }}</h3>
                                         </div>
                                     </div>
                                     
